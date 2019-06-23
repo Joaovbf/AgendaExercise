706ffbb68c4f4f549284ee6bce9d86f5cf46813c
Joaovbf <j.vitor.barros@hotmail.com>
2019-06-23 00:06:51 -0300

    definição dos metodos de inserção e remoção de itens da agenda

--- src/modelo/Agenda.java
+++ src/modelo/Agenda.java
@@ -6,11 +6,65 @@
 package modelo;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 /**
  *
  * @author joao
  */
 public class Agenda implements Serializable {
+    private ArrayList<ItemAgenda> colecaoTarefas;
+
+    public Agenda() {
+        this.colecaoTarefas = new ArrayList<>();
+    }
+
+    public Agenda(ArrayList<ItemAgenda> colecaoTarefas) {
+        this.colecaoTarefas = colecaoTarefas;
+    }
+
+    public ArrayList<ItemAgenda> getColecaoTarefas() {
+        return colecaoTarefas;
+    }
+
+    public void setColecaoTarefas(ArrayList<ItemAgenda> colecaoTarefas) {
+        this.colecaoTarefas = colecaoTarefas;
+    }
     
+    public ItemAgenda[] getItens(){
+        return (ItemAgenda[])this.getColecaoTarefas().toArray();
+    }
+    
+    public void inserir(ItemAgenda item){
+        int novoId = this.getItens()[this.getItens().length-1].getId() + 1;
+        item.setId(novoId);
+        this.colecaoTarefas.add(item);
+    }
+    
+    public boolean remover(int id){
+        ItemAgenda itemRemocao = null;
+        for (ItemAgenda colecaoTarefa : this.getColecaoTarefas()) {
+            if (colecaoTarefa.getId() == id) {
+                itemRemocao = colecaoTarefa;
+                break;
+            }
+        }
+        if (itemRemocao == null) 
+            return false;
+        
+        this.colecaoTarefas.remove(itemRemocao);
+        return true;
+    }
+    
+    public boolean remover(ItemAgenda item){
+        return this.colecaoTarefas.remove(item);
+    }
+    
+    public boolean gravar(){
+        return true;
+    }
+    
+    public boolean ler(){
+        return true;
+    }
 }
